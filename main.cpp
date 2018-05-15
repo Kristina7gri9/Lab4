4ad2e56253d64acb4fbdd51579b6260df749f29e
u211-16 <u211-16@public.mpei.local>
2018-05-15 10:25:41 +0300

    добавили функцию для вывода массива байтов и сделала проверку

--- main.cpp
+++ main.cpp
@@ -6,11 +6,30 @@ char nibble_to_hex(uint8_t i){
     assert(0x0 <= i && i <= 0xf);
     char digits[]="0123456789abcdef";
     return digits[i];}
-void print_to_hex(uint8_t byte)
+void print_in_hex(uint8_t byte)
 {
     cout<<nibble_to_hex(byte>>4);
     cout<<nibble_to_hex(byte&0b00001111);}
+const uint8_t* as_bytes(const void* data) {
+    return reinterpret_cast<const uint8_t*>(data);
 }
+void
+print_in_hex(const void* data, size_t size) {
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++) {
+        print_in_hex(bytes[i]);
+
+        // Для удобства чтения: пробелы между байтам, по 16 байт на строку.
+        if ((i + 1) % 16 == 0) {
+            cout << '\n';
+        }
+        else {
+            cout << ' ';
+        }
+    }
+}
+
+
 int main(){
     assert(nibble_to_hex(0x0) == '0');
     assert(nibble_to_hex(0x1) == '1');
@@ -28,5 +47,8 @@ int main(){
     assert(nibble_to_hex(0xd) == 'd');
     assert(nibble_to_hex(0xe) == 'e');
     assert(nibble_to_hex(0xf) == 'f');
-
+    uint32_t u32 = 0x42;
+    cout << "u32 bytes: ";
+    print_in_hex(&u32, sizeof(u32));
+    cout << '\n';
     return 0;}
